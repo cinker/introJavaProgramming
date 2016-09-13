1fc8f8b3241a17a91a702319025a7cd7760a842e
cinkeredu <cinker@england.edu>
2016-09-13 22:42:05 +0800

    Merge remote-tracking branch 'origin/master'

--- Chap06/ex6_31.java
+++ Chap06/ex6_31.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  */
 public class ex6_31 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+/*        Scanner input = new Scanner(System.in);
         System.out.println("Enter a credit card number as a long integer:");
         long card_number = input.nextLong();
         if(isValid(card_number))
@@ -16,18 +16,17 @@ public class ex6_31 {
         {
             System.out.println(card_number + " is invalid");
         }
+        */
+        System.out.print(getSize(55564));
+        System.out.print(sumOfOddPlace(55564));
     }
 
     public static boolean isValid(long number) {
-
+        return true;
     }
 
     public static int sumOfDoubleEvenPlace(long number){
-        int i = 0;
-
-        while(i >= 0) {
-
-        }
+        return 0;
     }
 
     public static int getDigit(int number){
@@ -40,18 +39,34 @@ public class ex6_31 {
     }
 
     public static int sumOfOddPlace(long number){
+        int sum = 0;
+        int size = 4;
 
+        while(size > 3)
+        {
+            size = getSize(number);
+            sum += number/Math.pow(10, size-1);
+            number = number % ((long)Math.pow(10, (size-2)));
+        }
+        return sum;
     }
 
     public static boolean prefixMatched(long number, int d) {
-
+        return true;
     }
 
     public static int getSize(long d){
+        int count = 0;
 
+        while (d > 0)
+        {
+            d = d/10;
+            count++;
+        }
+        return count;
     }
 
     public static long getPrefix(long number, int k){
-
+        return 0;
     }
 }
