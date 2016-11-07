68853e8d69196ef4d883edb9d3c15af8a696cfc7
Teng <cdoors@gmail.com>
2016-11-07 16:50:45 +0800

    test

---
+++ Chap31/IdentifyHostNameIP.java
@@ -0,0 +1,20 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by xute02 on 2016/11/4.
+ */
+public class IdentifyHostNameIP {
+    public static void main(String[] args) {
+        for (int i = 0; i<args.length; i++) {
+            try {
+                InetAddress address = InetAddress.getByName(args[i]);
+                System.out.print("Host name: " + address.getHostName());
+                System.out.println("IP address: " + address.getHostAddress());
+            }
+            catch (UnknownHostException ex) {
+                System.err.println("Unknown host or IP address: " + args[i]);
+            }
+        }
+    }
+}
